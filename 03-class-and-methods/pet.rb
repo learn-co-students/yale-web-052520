240e3aec010c288e697efaf4ffd32fc0f4218d39
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-05-27 10:56:58 -0500

    done

--- 03-class-and-methods/pet.rb
+++ 03-class-and-methods/pet.rb
@@ -4,9 +4,36 @@ class Pet
     # getter and setter
     attr_accessor :name, :type
 
+    # PETS = []
+    @@all = []
+
     def initialize(petType: type, petName: "fido")
         @type = petType
         @name = petName
+
+        @@all << self
+        # PETS << self
+    end
+
+
+    def display_info
+        # puts self.name
+        # puts name
+        # puts @name
+        # puts self.@name # syntax error
+        puts "Hi, I am a #{@type}, My name is #{@name}"
+        return self
+    end
+
+    def self.display_info
+        puts self
+        puts "I am printing from a class method"
+    end
+
+
+    def self.all
+        @@all
+        # PETS
     end
 
 end
@@ -14,6 +41,9 @@ end
 pet1 = Pet.new({petName: "ruby", petType: "dog"})
 pet2 = Pet.new(petName: "roxy", petType: "cat")
 
+NUM = 10
+
+PERSON = []
 
 binding.pry
 0
