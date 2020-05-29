5237034b997b1301ffd24698ca3d9d40e85562b2
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-05-29 09:29:24 -0500

    middle lecture code

--- 05-many-many/run.rb
+++ 05-many-many/run.rb
@@ -1,8 +1,15 @@
 require 'pry'
+require 'require_all'
 
-require_relative 'lib/doctor.rb'
-require_relative 'lib/patient.rb'
-require_relative 'lib/appointment.rb'
+require_all 'lib'
+
+# require_relative 'lib/doctor.rb'
+# require_relative 'lib/patient.rb'
+# require_relative 'lib/appointment.rb'
+
+d1 = Doctor.new("Dr. William")
+d2 = Doctor.new("Dr. Jeannifer")
+
 
 
 binding.pry
