0c6c295cca8db7834819668aec982c83cc7167ad
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-05-29 10:47:22 -0500

    lecture code

--- 05-many-many/lib/patient.rb
+++ 05-many-many/lib/patient.rb
@@ -14,10 +14,10 @@ class Patient
     end
 
     def appointments
-        Appointment.all.select {|appointment| appointment.patient == self}
+        Appointment.all.select {|appointment| appointment.patient == self }
     end
 
     def doctors
-        appointments.map {|appointment| appointment.doctor}
+        appointments.map {|appointment| appointment.doctor }
     end
 end
