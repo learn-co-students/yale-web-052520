53f1d065f23e4d4ab30e5fd5c425e2775fbea955
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-06-15 10:53:05 -0500

    done

--- 12-Sinatra-MVC/app/controllers/application_controller.rb
+++ 12-Sinatra-MVC/app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < Sinatra::Base
 
+    set :views, "app/views"
+
+    get '/' do
+        "Hello World"
+    end
+
+    get '/home' do
+       erb :home
+    end
+
+    # display list of all books
+    get '/books' do
+        # @name = "Vidhi"
+
+        @books = Book.all
+        erb :index
+    end
+
+    # display a single book
+    get '/books/:id' do
+        # binding.pry
+        @book = Book.find(params[:id])
+        erb :show
+    end
+
+
 end
