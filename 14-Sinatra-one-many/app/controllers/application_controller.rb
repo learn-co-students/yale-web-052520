1bd62c807dbaf142d4e8ae91b8b43fe75dfc67e0
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-06-17 10:54:24 -0500

    done

--- 14-Sinatra-one-many/app/controllers/application_controller.rb
+++ 14-Sinatra-one-many/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < Sinatra::Base
 
     set :method_override, true
+    # set :views, "app/views"
   
 end
--- 14-Sinatra-one-many/app/controllers/dogs_controller.rb
+++ 14-Sinatra-one-many/app/controllers/dogs_controller.rb
@@ -5,7 +5,14 @@ class DogsController < ApplicationController
     # display list of all dogs
     get '/dogs' do
         @dogs = Dog.all
+        # erb :"dogs/index"
         erb :index
+    end
+
+    # create a dog form
+    get '/dogs/new' do
+        @owners = Owner.all
+        erb :new
     end
 
     # display a dog
@@ -13,5 +20,27 @@ class DogsController < ApplicationController
         @dog = Dog.find(params[:id])
         erb :show
     end 
+
+    # create a dog
+    post "/dogs" do
+        # binding.pry
+        # .create => .new + .save
+        # dog = Dog.create(params)
+
+        dog = Dog.new(params[:dog])
+
+        unless params[:owner][:name].empty?
+            new_owner = Owner.create(params[:owner])
+
+            dog.owner_id = new_owner.id
+            # dog.owner = new_owner
+        end
+
+        dog.save
+
+        redirect "/dogs/#{dog.id}"
+    end
+
+  
 
 end
