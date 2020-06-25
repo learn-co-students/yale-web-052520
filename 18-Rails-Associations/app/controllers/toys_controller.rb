e1a25ebcc3ea35917fd0e02525b5271c5420dc6b
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-06-25 17:18:26 -0500

    done

--- 18-Rails-Associations/app/controllers/toys_controller.rb
+++ 18-Rails-Associations/app/controllers/toys_controller.rb
@@ -16,7 +16,17 @@ class ToysController < ApplicationController
 
     def create
         # byebug
-        toy = Toy.create(toys_params)
+
+        toy = Toy.new(toys_params)
+
+        #check if user is writting dog's name or not
+        unless params[:toy][:dog][:name].empty? 
+            dog = Dog.create(dogs_params) # calling strongs params
+            
+            toy.dog_id = dog.id
+        end
+
+        toy.save
         redirect_to "/toys/#{toy.id}"
     end
 
@@ -24,6 +34,11 @@ class ToysController < ApplicationController
 
     def toys_params
         params.require(:toy).permit(:name, :dog_id) #strong_params
+    end
+
+    def dogs_params
+        #strong_params for params[:toy][:dog][:name]
+        params.require(:toy).permit({:dog => [:name]})[:dog]
     end
 
     
