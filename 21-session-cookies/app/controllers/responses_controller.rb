7ab70dc4f56947b9b6314d6ceb51e1894d7bfafa
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-06-29 10:10:43 -0500

    done

--- 21-session-cookies/app/controllers/responses_controller.rb
+++ 21-session-cookies/app/controllers/responses_controller.rb
@@ -1,9 +1,17 @@
 class ResponsesController < ApplicationController
 
-    before_action :define_question, :define_response
+    before_action :define_question, :define_response, :define_number_correct, :display_message
 
     def new
 
+    end
+
+    def define_number_correct
+        @number_correct = session[:number_correct]
+    end
+
+    def display_message
+        @message = flash[:message]
     end
 
     def define_question
@@ -15,19 +23,22 @@ class ResponsesController < ApplicationController
     end
 
     def create
-        @number_correct = 0
+        if session[:number_correct] == nil
+            session[:number_correct] = 0
+        end
         response = Response.create(response_params)
         if response.question.correct_answer == response.answer
-            @number_correct = @number_correct + 1
-            puts "Correct"
+            session[:number_correct] = session[:number_correct] + 1
+            flash[:message] =  "Your previous answer was Correct"
         else
-            puts "Incorrect"
+            flash[:message] = "Your previous answer was Incorrect"
         end
+        puts "Answered Correctly: #{session[:number_correct]}"
         redirect_to '/random-question'
     end
 
     private
-    
+
     def response_params
         params.require(:response).permit(:answer_id, :question_id)
     end
